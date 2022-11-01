382c7abd3449d765570af71fcbb381cb4cf17247
Franco Poffo <franco_sutter@yahoo.com.br>
2022-11-01 14:47:41 -0300

    Adicionando JS: operações e computação.

--- js/app.js
+++ js/app.js
@@ -23,15 +23,45 @@ class Calculadora {
     }
 
     escolheOperacao(operacao){
+        if (this.operandoAtual === '') return
+        if (this.operandoAnterior !== ''){
+            this.computa()
+        }
+        this.operacao = operacao
+        this.operandoAnterior = this.operandoAtual
+        this.operandoAtual = ''
 
     }
 
     computa(){
-
+        let computacao
+        const ant = parseFloat(this.operandoAnterior)
+        const atual = parseFloat(this.operandoAtual)
+        if (isNaN(ant) || isNaN(atual)) return
+        switch (this.operacao) {
+            case '+':
+                computacao = ant + atual
+                break
+            case '-':
+                computacao = ant - atual
+                break
+            case '*':
+                computacao = ant * atual
+                break
+            case '÷':
+                computacao = ant / atual
+                break
+            default:
+                return
+        }
+        this.operandoAtual = computacao
+        this.operacao = undefined
+        this.operandoAnterior = ''
     }
 
     atualizaTela(){
         this.operandoAtualTextElement.innerText = this.operandoAtual
+        this.operandoAnteriorTextElement.innerText = this.operandoAnterior
     }
 
 }
@@ -40,7 +70,7 @@ const botaoNumero = document.querySelectorAll("#numero");
 const botaoOperacao = document.querySelectorAll("#operação");
 const botaoDelete = document.querySelector("#delete");
 const botaoIgual = document.querySelector("#igual");
-const botaoAC = document.querySelector("#all-clear");
+const botaoAC = document.querySelector("#allclear");
 const operandoAtualTextElement = document.querySelector("#atual");
 const operandoAnteriorTextElement = document.querySelector("#anterior");
 
@@ -52,4 +82,21 @@ botaoNumero.forEach(botao => {
         calculadora.escreveNumero(botao.innerText)
         calculadora.atualizaTela()
     })
+})
+
+botaoOperacao.forEach(botao => {
+    botao.addEventListener('click', () => {
+        calculadora.escolheOperacao(botao.innerText)
+        calculadora.atualizaTela()
+    })
+})
+
+botaoIgual.addEventListener('click', botao =>{
+    calculadora.computa()
+    calculadora.atualizaTela()
+})
+
+botaoAC.addEventListener('click', botao =>{
+    calculadora.limpa()
+    calculadora.atualizaTela()
 })
